4617f52df266340d43df1544f47e820831c3c85d
starr4ever@TransEdenV4 <starflowerr777@gmail.com>
2025-01-04 02:00:05 +0100

    prep stuff
    Signed-off-by: starr4ever@TransEdenV4 <starflowerr777@gmail.com>

---
+++ prep.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# fix the fucking versions for the cache (do it even on dev branch)
+# ..wait i should git actions this maybe?
+# copy dev stuff into temp directory into .gitignore zone or different branch
+# minify everything with some tool
+# git push
+# move dev stuff back
